9c6b9df04ad09dcbddc13dd7a3f93c4bfbf91796
heesanggod <sangin583@gmail.com>
2024-03-25 17:35:56 +0900

    24.03.25 17:50

--- basic/basic/src/main/java/com/sangin/basic/controller/BasicController.java
+++ basic/basic/src/main/java/com/sangin/basic/controller/BasicController.java
@@ -1,5 +1,6 @@
 package com.sangin.basic.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -8,6 +9,12 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.sangin.basic.service.BasicService;
+import com.sangin.basic.service.implement.BasicServiceImplement;
+
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
 // Controller 레이어 : 
     // - 클라이언트와 서버간의 접점
@@ -19,11 +26,37 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 // @RequestMapping() : HTTP 요청에 클래스와 메서드를 맵핑하기 위한 어노테이션
 // @RequestMapping(value = "/main", method = {RequestMethod.GET})
+
 // HTTP GET localhost:4000/main/**     >>이 주소에 관하여 위의 컨드롤러로 처리하겠다는 뜻
 @RequestMapping("/main")
+// final 로 선언된 필드 변수에 대해서 생성자를 자동으로 작성함
+@RequiredArgsConstructor
 public class BasicController {
+
+//    private BasicService service;
+
+// 의존성 역전 (DI) :
+// - 해당 클래스에 필요한 의존성을 의부에서(클래스의 인스턴스를 생성하는 위치)에서 주입
+// 1) 생성자를 통한 의존성 주입 2) setter 메서드를 통한 의존성 주입 3) 필드를 통한 의존성 주입
+// -> 1) 생성자를 통한 의존성 주입을 spring 에서 권장 (의존성이 주입되지 않은 경우가 존재할 수 없기 때문)
+// - Spring Framework 에서 의존성 주입은 제어의 역전 (IoC)를 통해서 spring framework 가 진행
+//      (@Component 를 사용하여 spring bean 으로 등록된 class에 한해서)
+
+    // @Autowired : 등록된 Spring Bean 을 제어의 역전을 통해서 의존성을 주입하는 방법
+    // - 단, 생성자를 통한 의존성 주입에는 @Autowired 를 생략해도 됨
+
+    // @Autowired : 생성자에서는 생략가능하도록 서비스를 구축해놓았다
+    // public BasicController(BasicService service) {
+    //     this.service = service;
+    // }
+
+    // 의존성 주입시, 클래스로 직접 참조변수를 만들지 않고 인터페이스로 간접적으로 만드는 이유? 
+    // => 고수준의 모듈에서 저수준의 모듈을 직접 참조하지 않고 추상화를 통해 간접 참조함으로써 각 모듈간의 결합도를 낮춤
+    //      -> 코드의 재사용성 향상, 유지보수성 향상
+    private final BasicService service;
     
     // HTTP GET localhost:4000/main/    >> 밑의 코드가 의미하는 곳
+
     @RequestMapping(value = "/", method = {RequestMethod.GET})
     public String getHello() {
         return "Hello Springboot!!";
@@ -34,6 +67,8 @@ public class BasicController {
     // @GetMapping() : RequestMapping 기능을 GET Method 에 한정시킨 것 (가독성 + 안정성)
     @GetMapping ("/apple")
     public String getApple () {
+        // BasicServiceImplement service = new BasicServiceImplement();
+        // return service.getApple();
         return "Get Mapping 으로 만든 메서드";
     }
 
---
+++ basic/basic/src/main/java/com/sangin/basic/service/implement/BasicServiceImplement.java
@@ -0,0 +1,28 @@
+package com.sangin.basic.service.implement;
+
+import org.springframework.stereotype.Service;
+
+import com.sangin.basic.service.BasicService;
+
+//  Service 레이어 : 
+// - 실제 비즈니스 로직(연산)을 실행하는 영역
+// - 트랜잭션 처리나 유효성 검사(직전 비즈니스  로직 결과에 대한)를 수행하기도 함
+// - Controller 로 부터 받은 요청 데이터에 대해서 필요에 따라 Repository 등을 거쳐 연산을 진행하고 Controller 에 응답에 대한 데이터 반환
+
+// Service : 해당 클래스를 Spring Bean 으로 등록하는 어노테니션, 역할은  @Component 와 동일
+// @Component : 해당 클래스를 Spring Bean 으로 등록하는 어노테이션
+// @Spring Bean : 제어의 역전을 통해서 의존성 주입시 해당 클래스의 인스턴스를 Spring Framework 가 제어하는 요소 .  Spring Framework 제어하는 인스턴스
+@Service  
+public class BasicServiceImplement implements BasicService {
+
+    @Override
+    public String getHello() {
+        return "Hello Springboot!!";
+    }
+
+	@Override
+	public String getApple() {
+        return "Get Mapping 으로 만든 메서드";
+	}
+    
+}
