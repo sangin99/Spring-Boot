6ead2effee0af13573cf8e657dfde3bba91e3274
heesanggod <sangin583@gmail.com>
2024-04-01 17:49:01 +0900

    Spring Boot 연습

--- board/src/main/java/com/example/board/config/CorsConfig.java
+++ board/src/main/java/com/example/board/config/CorsConfig.java
@@ -5,10 +5,10 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig implements WebMvcConfigure {
+public class CorsConfig implements WebMvcConfigurer {
 
     @Override
-    public void addCorsMappings (CorsRegistry registry) {
+    public void addCorsMappings(CorsRegistry registry) {
         registry
             .addMapping("/**")
             .allowedMethods("*")
