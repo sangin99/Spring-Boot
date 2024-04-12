c5f4220f05d17830bcdb15cb7016c5956557e156
heesanggod <sangin583@gmail.com>
2024-04-12 14:54:04 +0900

    JWT 비밀키 숨기기

--- basic/basic/src/main/java/com/sangin/basic/provider/JwtProvider.java
+++ basic/basic/src/main/java/com/sangin/basic/provider/JwtProvider.java
@@ -6,6 +6,7 @@ import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import java.util.Date;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import io.jsonwebtoken.Claims;
@@ -23,6 +24,27 @@ import io.jsonwebtoken.security.Keys;
 // - 서명 : 헤더와 페이로드를 합쳐서 인코딩하고 지정한 비밀키로 암호화한 데이터
 @Component
 public class JwtProvider {
+
+    // secretKey 의 값을 application.properties 로 지정
+    // JWT 암호화에 사용되는 비밀키는 보안 관리가 되어야함
+    // 코드에 직접적으로 비밀키를 작성하는 것은 보안상 좋지 않음
+    
+    // 해결책?
+    // 1. application.properties / application.yaml 에 등록
+    // - application.properties 혹은 application.yaml 에 비밀키 작성
+    // - @Value()를 이용하여 데이터를 가져옴
+    // - ※주의사항※ : application.properties / application.yaml 을 .gitignore 에 등록해야함 
+    @Value("${jwt.secret-key}")
+    private String secretKey;
+
+    // 2. 시스템의 환경변수로 등록해서 사용 (1.의 방법보다 보안상 안전)
+    // - OS 자체의 시스템 환경 변수에 비밀키를 작성
+    // - Spring 에서 Environment 객체를 이용하여 값을 가져옴
+
+    // 3. 외부 데이터 관리 도구를 사용
+    // - 해당 서버가 아닌 타 서버에 등록된 Vault 도구를 사용하여 비밀키를 관리
+    // - OS 부팅시에 Vault 서버에 접근하여 비밀키를 가져와서 사용하는 방법
+    // - 매 부팅시 다른 비밀키를 제공해줌
     
     //? JWT 생성
     public String create(String principle) {
@@ -30,7 +52,7 @@ public class JwtProvider {
         Date expiredDate = Date.from(Instant.now().plus(4, ChronoUnit.HOURS)); 
                                     //현재시간에 대한 Instant 생성.plus(4시간 뒤)
         // 비밀키 생성
-        Key key = Keys.hmacShaKeyFor("qwerasdfzxcvqwerasdfzxcvqwerasdfzxcvqwerasdfzxcv".getBytes(StandardCharsets.UTF_8));
+        Key key = Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
 
         // JWT 생성
         String jwt = Jwts.builder()
@@ -54,7 +76,7 @@ public class JwtProvider {
         // jwt 검증 결과로 나타나는 페이로드가 저장될 변수
         Claims claims = null;
         // 비밀키 생성
-        Key key = Keys.hmacShaKeyFor("qwerasdfzxcvqwerasdfzxcvqwerasdfzxcvqwerasdfzxcv".getBytes(StandardCharsets.UTF_8));
+        Key key = Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
 
         try {
             // 비밀키로 jwt 복호화 작업
@@ -69,6 +91,6 @@ public class JwtProvider {
         }
 
         return claims.getSubject();
-        
+
     }
 }
